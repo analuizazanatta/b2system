d7c58839852fa96a8a292af86bc2817bf27154ac
GabrielHvignolli <gabriel.vignolli@gmail.com>
2024-06-10 20:59:28 -0300

    Exercicios aula 10/06/2024

---
+++ exercicios-sql-10062024.sql
@@ -0,0 +1,187 @@
+--Passo 02 Nova atividade do dia "10-06-2024" 
+--Atividade 02
+-- Resolver os exercicios abaixo usando os comandos sql passados na ultima aula. Antes de iniciar a resolução
+
+
+--Exercicio 01
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor ou igual a 7.
+select * from PRODUTO where id <= 7;
+
+--Exercicio 02
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor ou igual a 15.
+select * from PRODUTO where id <= 15;
+
+--Exercicio 03
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor ou igual a 23.
+select * from PRODUTO where id <= 23;
+
+--Exercicio 04
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor ou igual a 8.
+select * from PRODUTO where id <= 8;
+
+--Exercicio 05
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor ou igual a 6.
+select * from PRODUTO where id <= 6;
+
+--Exercicio 06
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior ou igual a 7.
+select * from PRODUTO where id >= 7;
+
+--Exercicio 07
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior ou igual a 15.
+select * from PRODUTO where id >= 15;
+
+--Exercicio 08
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior ou igual a 23.
+select * from PRODUTO where id >= 23;
+
+--Exercicio 09
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior ou igual a 8.
+select * from PRODUTO where id >= 8;
+
+--Exercicio 10
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" igual a 6.
+select * from PRODUTO where id = 6;
+
+--Exercicio 11
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" igual a 15.
+select * from PRODUTO where id = 15;
+
+--Exercicio 12
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" igual a 23.
+select * from PRODUTO where id = 23;
+
+--Exercicio 13
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" igual a 8.
+select * from PRODUTO where id = 8; 
+
+--Exercicio 14
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" igual a 6.
+select * from PRODUTO where id = 6;
+
+--Exercicio 15
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" igual a 12.
+select * from PRODUTO where id = 12;
+
+--Exercicio 16
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" diferente de 15.
+select * from PRODUTO where id <> 15;
+
+--Exercicio 17
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" diferente de 23.
+select * from PRODUTO where id <> 23;
+
+--Exercicio 18
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" diferente de 8.
+select * from PRODUTO where id <> 8;
+
+--Exercicio 19
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" diferente de 6.
+select * from PRODUTO where id <> 6;
+
+--Exercicio 20
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" diferente de 12.
+select * from PRODUTO where id <> 12;
+
+--Exercicio 21
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior que 15.
+select * from PRODUTO where id > 15;
+
+--Exercicio 22
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior que 23.
+select * from PRODUTO where id > 23;
+
+--Exercicio 23
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior que 8.
+select * from PRODUTO where id > 8;
+
+--Exercicio 24
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior que 6.
+select * from PRODUTO where id > 6;
+
+--Exercicio 25
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" maior que 12.
+select * from PRODUTO where id > 12;
+
+--Exercicio 26
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor que 15.
+select * from PRODUTO where id < 15;
+
+--Exercicio 27
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor que 23.
+select * from PRODUTO where id < 23;
+
+--Exercicio 28
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor que 8.
+select * from PRODUTO where id < 8;
+
+--Exercicio 29
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" menor que 6.
+select * from PRODUTO where id < 6;
+
+--Exercicio 30
+--Crie um comando SQL usando o comando "between", que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" entre 4 e 12.
+select * from PRODUTO where id between 4 and 12;
+
+--Exercicio 31
+--Crie um comando SQL usando o comando "between", que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" entre 4 e 15.
+select * from PRODUTO where id between 4 and 15;
+
+--Exercicio 32
+--Crie um comando SQL usando o comando "between", que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" entre 4 e 23.
+select * from PRODUTO where id between 4 and 23;
+
+--Exercicio 33
+--Crie um comando SQL usando o comando "between", que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" entre 4 e 8.
+select * from PRODUTO where id between 4 and 8;
+
+--Exercicio 34
+--Crie um comando SQL usando o comando "between", que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" entre 4 e 6.
+select * from PRODUTO where id between 4 and 6;
+
+--Exercicio 35
+--Crie um comando SQL usando o comando "between", que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" entre 4 e 12.
+select * from PRODUTO where id between 4 and 12;
+
+--Exercicio 36
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" contido em 4 e 15.
+select * from PRODUTO where id in (4, 15);
+
+--Exercicio 37
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" contido em 4 e 23.
+select * from PRODUTO where id in (4, 23);
+--Exercicio 38
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" contido em 4 e 8.
+select * from PRODUTO where id in (4, 8);
+
+--Exercicio 39
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" contido em 4 e 6.
+select * from PRODUTO where id in (4, 6);
+
+--Exercicio 40
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" contido em 4 e 12.
+select * from PRODUTO where id in (4, 12);
+
+--Exercicio 41
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" nao contido em 4 e 15.
+select * from PRODUTO where id not in (4, 15);
+
+--Exercicio 42
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" nao contido em 4 e 23.
+select * from PRODUTO where id not in (4, 23);
+
+--Exercicio 43
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" nao contido em 4 e 8.
+select * from PRODUTO where id not in (4, 8);
+
+--Exercicio 44
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" nao contido em 4 e 6.
+select * from PRODUTO where id not in (4, 6);
+
+--Exercicio 45
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" nao contido em 4 e 12.
+select * from PRODUTO where id not in (4, 12);
+
+--Exercicio 45
+--Crie um comando SQL que retorne apenas os registros da tabela "PRODUTO" que tiverem o "id" nao contido em 4 e 12.
+select * from PRODUTO where id not in (4, 12);
